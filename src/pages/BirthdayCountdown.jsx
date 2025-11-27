a5bbf54f5019c23246ba96040a1d5c32677f6bcc
abubakar shiddiq <abenk223jr@gmail.com>
2025-11-27 22:31:43 +0800

    baru terus

--- src/pages/BirthdayCountdown.jsx
+++ src/pages/BirthdayCountdown.jsx
@@ -14,7 +14,7 @@ const BirthdayCountdown = ({ isIOS }) => {
   const [replyMessage, setReplyMessage] = useState("");
   const navigate = useNavigate();
 
-  const targetDate = new Date("2025-11-27T23:00:00");
+  const targetDate = new Date("2025-11-27T22:00:00");
 
   useEffect(() => {
     const timer = setInterval(() => {
