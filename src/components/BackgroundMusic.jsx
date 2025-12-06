44b64a297c56b36d91b8916e257ce44bd9ce7600
abubakar shiddiq <abenk223jr@gmail.com>
2025-12-06 17:09:59 +0800

    baru

--- src/components/BackgroundMusic.jsx
+++ src/components/BackgroundMusic.jsx
@@ -48,7 +48,7 @@ const BackgroundMusic = () => {
           ref={iframeRef}
           width="1"
           height="1"
-          src="https://www.youtube.com/embed/tGv7CUutzqU?autoplay=1&loop=1&playlist=tGv7CUutzqU&enablejsapi=1&mute=0"
+          src="https://www.youtube.com/embed/gfu7Eylg18M?autoplay=1&loop=1&playlist=gfu7Eylg18M&enablejsapi=1&mute=0"
           title="Background Music"
           frameBorder="0"
           allow="autoplay; encrypted-media"
